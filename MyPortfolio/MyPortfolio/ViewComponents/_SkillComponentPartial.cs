eab82ca00a1371fe328c80ccb79996771d868b85
Özgür ERCİYAS <zgrrcys1999@gmail.com>
2024-10-19 00:26:21 +0300

    MyPortfolioWebsiteV2
    MyPortfolioWebsitePart2

---
+++ MyPortfolio/MyPortfolio/ViewComponents/_SkillComponentPartial.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using MyPortfolio.DAL.Context;
+
+namespace MyPortfolio.ViewComponents
+{
+    public class _SkillComponentPartial : ViewComponent
+    {
+        MyPortfolioContext context = new MyPortfolioContext();
+        public IViewComponentResult Invoke()
+        {
+            var value = context.Skills.ToList();
+            return View(value);
+        }
+    }
+}
